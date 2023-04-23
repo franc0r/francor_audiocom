8edba97ed1b57afcc997f65b264bfed32d848aa4
Tobias Zech <zech.tobias@gmail.com>
2023-04-23 21:10:36 +0200

    added client config start

--- client.py
+++ client.py
@@ -1,35 +1,47 @@
+import sys
 import socket, cv2, pickle,struct,time
 import pyshine as ps
 
-mode =  'get'
-name = 'CLIENT RECEIVING AUDIO'
-audio,context = ps.audioCapture(mode=mode)
-ps.showPlot(context,name)
+class AudioClient:
 
-# create socket
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '192.168.178.51'
-port = 4982
+	def __init__(self, host_ip, port, showui=False):
+		mode =  'get'
+		name = 'CLIENT RECEIVING AUDIO'
+		self.audio,context = ps.audioCapture(mode=mode)
+		if showui:
+			ps.showPlot(context,name)
 
-socket_address = (host_ip,port)
-client_socket.connect(socket_address) 
-print("CLIENT CONNECTED TO",socket_address)
-data = b""
-payload_size = struct.calcsize("Q")
-while True:
-	while len(data) < payload_size:
-		packet = client_socket.recv(4*1024) # 4K
-		if not packet: break
-		data+=packet
-	packed_msg_size = data[:payload_size]
-	data = data[payload_size:]
-	msg_size = struct.unpack("Q",packed_msg_size)[0]
-	
-	while len(data) < msg_size:
-		data += client_socket.recv(4*1024)
-	frame_data = data[:msg_size]
-	data  = data[msg_size:]
-	frame = pickle.loads(frame_data)
-	audio.put(frame)
+		# create socket
+		self.client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		port = 4982
 
-client_socket.close()
+		socket_address = (host_ip,port)
+		self.client_socket.connect(socket_address) 
+		print("CLIENT CONNECTED TO",socket_address)
+
+	def run(self):
+		
+		data = b""
+		payload_size = struct.calcsize("Q")
+		while True:
+			while len(data) < payload_size:
+				packet = self.client_socket.recv(4*1024) # 4K
+				if not packet: break
+				data+=packet
+			packed_msg_size = data[:payload_size]
+			data = data[payload_size:]
+			msg_size = struct.unpack("Q",packed_msg_size)[0]
+			
+			while len(data) < msg_size:
+				data += self.client_socket.recv(4*1024)
+			frame_data = data[:msg_size]
+			data  = data[msg_size:]
+			frame = pickle.loads(frame_data)
+			self.audio.put(frame)
+
+			self.client_socket.close()
+
+if __name__ == '__main__':
+	params = sys.argv[1:]
+	audio_client = AudioClient(*params)
+	audio_client.run()
--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import json
 import multiprocessing
 import subprocess
 
-def start_server(ip, port, showUI):
+def start_server(ip, port, showui):
     try:
-        print(ip, port, showUI)
-        subprocess.run(["python", "server.py", str(ip), str(port), str(showUI)])
+        subprocess.run(["python", "server.py", str(ip), str(port), str(showui)])
     except:
-        subprocess.run(["python3", "server.py", str(ip), str(port), str(showUI)])
+        subprocess.run(["python3", "server.py", str(ip), str(port), str(showui)])
 
-def start_client():
+def start_client(ip, port, showui):
     try:
-        subprocess.run(["python", "client.py"])
+        subprocess.run(["python", "client.py", str(ip), str(port), str(showui)])
     except:
-        subprocess.run(["python3", "client.py"])
+        subprocess.run(["python3", "client.py", str(ip), str(port), str(showui)])
 
 def follow_routine_basestation():
     pass
@@ -30,14 +29,14 @@ def main():
     if "Empty" == sys.argv[1]:
         print("started dummy process")
 
+    if 's' in params['mode']:
+        server_process = multiprocessing.Process(target=start_server, args=(params["HostIP"],params["Port"],True))
+        server_process.start()
+        server_process.join()        
     if 'r' in params['mode']:
-        client_process = multiprocessing.Process(target=start_client)
+        client_process = multiprocessing.Process(target=start_client, args=(params["ReceiverIP"],params["Port"],True))
         client_process.start()
         client_process.join()
-    if 's' in params['mode']:
-        server_process = multiprocessing.Process(target=start_server, args=(params["IP","Port",True]))
-        server_process.start()
-        server_process.join()
 
 if __name__ == '__main__':
     main()
